7f7c77c67275a7cdf2a2a6b6a35ccae641efe226
Harshita-Raghu <harshita.raghu@nividapps.com>
2020-09-15 20:40:00 +0530

    First Commit

---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+export default function HomeScreen() {
+  return (
+    <View>
+      <Text>Home page</Text>
+    </View>
+  );
+}
